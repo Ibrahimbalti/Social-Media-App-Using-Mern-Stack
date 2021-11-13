8f9050f63e5b0d800f698253d0344ac8c626c281
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-11-13 12:22:12 +0500

    delete user account

--- api/routers/users.js
+++ api/routers/users.js
@@ -30,8 +30,18 @@ router.put('/:id', async (req, res) => {
 });
 //delete user
 
-// router.delete('/:id', async (req, res) => {
-// }
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json('Delete account successfully');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('You can delete only your account');
+  }
+});
 
 //get user
 // followers user
