5b50ac192a7b11dfa81bd05d3a0cbd2b1874e1f5
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-11-14 16:20:02 +0500

    complete the follower api

--- api/routers/users.js
+++ api/routers/users.js
@@ -57,6 +57,27 @@ router.get('/:id', async (req, res) => {
 });
 
 // followers user
+
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.body.userId } });
+        res.status(200).json('User has been followed');
+      } else {
+        res.status.apply(403).json('You already follow this user');
+      }
+    } catch (err) {
+      res.status(500).json({ err: 'server Error' });
+    }
+  } else {
+    res.status(403).json("You can't follow your self");
+  }
+});
 // unfollow user
 
 module.exports = router;
