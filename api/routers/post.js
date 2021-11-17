b4fda14e9fccaf0551ce68c9c63e56cf87452d1d
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-11-17 12:41:50 +0500

    delete post

--- api/routers/post.js
+++ api/routers/post.js
@@ -27,6 +27,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 //delete post
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json('Post has been deleted');
+    } else {
+      res.status(403).json('You can only delete your post');
+    }
+  } catch (err) {
+    res.status(500).json({ err: 'server error' });
+  }
+});
 //like a post
 //get a post
 //get timeline post
